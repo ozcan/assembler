7c5871c7c8bfd609f1a62d0a4cf2587b2726a314
Özcan Esen <ozcanesen@gmail.com>
2015-11-09 18:44:27 +0200

    improve performance using buffering kmers in each go routine

--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
-    "fmt"
+    _ "fmt"
     "log"
     "os"
     "bufio"
     "sync"
     "flag"
+    "strconv"
+    "runtime/pprof"
 )
 
 var kmer_size int
-var kmer_counts = make(map[string]int)
-var kmer_queue = make(chan string, 10000)
+var kmer_counts = make(map[string]uint32)
+var kmer_queue = make(chan map[string]uint32)
 var wg sync.WaitGroup
+ 
 
 func main() {
     kmerPtr := flag.Int("k", 27, "k-mer size")
     fastaPtr := flag.String("f", "", "fasta file")
+    outputPtr := flag.String("o", "kmers.gob", "output file")
+    cpuprofilePtr := flag.String("cpuprofile", "", "write cpu profile to file")
     flag.Parse()
+
+    if *cpuprofilePtr != "" {
+        f, err := os.Create(*cpuprofilePtr)
+        if err != nil {
+            log.Fatal(err)
+        }
+        pprof.StartCPUProfile(f)
+        defer pprof.StopCPUProfile()
+    }
 
     kmer_size = *kmerPtr
     file, err := os.Open(*fastaPtr)
@@ -30,38 +44,70 @@ func main() {
     go store_kmers()
 
     scanner := bufio.NewScanner(file)
+    var sbuff []string
     for scanner.Scan() {
         if (i % 4 == 1) {
+            // fasta file contains new data in each 4 lines.
+            sbuff = append(sbuff, scanner.Text())
+        }
+
+        if (i % 40000 == 1) {
             wg.Add(1)
-            go get_kmers(scanner.Text())
+            go get_kmers(sbuff)
+            sbuff = nil
+
         }
 
         i += 1
     }
-
+    // rest
+    wg.Add(1)
+    go get_kmers(sbuff)
     wg.Wait()
-
-    for kmer := range kmer_counts {
-        fmt.Printf("%s\n>%d\n", kmer, kmer_counts[kmer])
-    }
+    write_to_disk(outputPtr)
 
     if err := scanner.Err(); err != nil {
         log.Fatal(err)
     }
 }
 
-func store_kmers() {
-    for kmer := range kmer_queue {
-        kmer_counts[kmer]++
+func write_to_disk(outputPtr *string) {
+    if *outputPtr != "" {
+        f, err := os.Create(*outputPtr)
+        if err != nil {
+            log.Fatal(err)
+        }
+        writer := bufio.NewWriter(f)
+        for kmer := range kmer_counts {
+            writer.WriteString(kmer + "\n" + strconv.Itoa(int(kmer_counts[kmer])) + "\n")
+        }
+
+        writer.Flush()
+        f.Close()
     }
 }
 
-func get_kmers(read string) {
-    length := len(read)
+func store_kmers() {
+    // merge local results in single map
+    for local_kmer_result := range kmer_queue {
+        for kmer, val := range local_kmer_result {
+            kmer_counts[kmer] = kmer_counts[kmer] + val
+        }
+    }
+}
+
+func get_kmers(reads []string) {
+    var local_kmer_counts = make(map[string]uint32)
     
-    for i := 0; i < length - kmer_size; i++ {
-        kmer_queue <- read[i:i+kmer_size]
+    for read_index := range reads {
+        read := reads[read_index]
+        length := len(read)
+
+        for i := 0; i < length - kmer_size; i++ {
+            local_kmer_counts[read[i:i+kmer_size]]++
+        }
     }
 
+    kmer_queue <- local_kmer_counts
     wg.Done()
 }
